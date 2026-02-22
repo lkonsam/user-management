96d7940dea143cf338ec1279ead53ef980b9bb57
Lkonsam <lkonsam@gmail.com>
2026-02-22 22:37:07 +0530

    Update api.ts, auth.api.ts as login doesn't need token

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+export const API_BASE_URL = "http://localhost:5000/api/v1";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -1,7 +1,9 @@
-import API from "./api";
+import axios from "axios";
+import {API_BASE_URL} from "./api";
 import type { LoginResponse } from "../types/auth.types";
 
+//login function doesn't need to use API instance because it doesn't require the token in the header
 export const login = async (email: string, password: string) => {
-  const res = await API.post<LoginResponse>("/auth/login", { email, password });
+  const res = await axios.post<LoginResponse>(`${API_BASE_URL}/auth/login`, { email, password });
   return res.data;
 };
